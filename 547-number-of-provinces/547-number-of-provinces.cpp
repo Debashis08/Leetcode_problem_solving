1a87fd1312b764f2aa9bcef202b4519c77207f54
Debashis Nandi <debashisnandi576@gmail.com>
2022-06-12 19:46:14 +0530

    Time: 22 ms (93.22%), Space: 14.7 MB (16.44%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -0,0 +1,44 @@
+class Solution 
+{
+private:
+    vector<int> visited;
+    vector<vector<int>> graph;
+    int no_of_provinces=0;
+    void dfs_visit(int u)
+    {
+        visited[u]=1;
+        // for(auto i:graph[u])
+        // {
+        //     if(i==1 && visited[i]==0)
+        //     {
+        //         dfs_visit(i);
+        //     }
+        // }
+        for(int i=0;i<graph.size();i++)
+        {
+            if(i!=u && visited[i]==0 && graph[u][i]==1)
+            {
+                dfs_visit(i);
+            }
+        }
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) 
+    {
+        graph=isConnected;
+        visited.resize(graph.size());
+        for(int i=0;i<graph.size();i++)
+        {
+            visited[i]=0;
+        }
+        for(int i=0;i<graph.size();i++)
+        {
+            if(visited[i]==0)
+            {
+                dfs_visit(i);
+                no_of_provinces++;
+            }
+        }
+        return no_of_provinces;
+    }
+};
