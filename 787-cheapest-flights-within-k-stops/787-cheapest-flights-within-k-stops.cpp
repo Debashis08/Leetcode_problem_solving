4ced7ba7d795cd8e99d5ae1d02dd66795654fa36
Debashis Nandi <debashisnandi576@gmail.com>
2022-06-21 18:31:58 +0530

    Time: 53 ms (46.01%), Space: 14 MB (43.08%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,53 +1,39 @@
-class Solution {
-    
+class Solution 
+{
+private:
+    map<int,vector<pair<int,int>>> adjlist;
+    vector<int> distance;
+    queue<tuple<int,int,int>> q_obj;
 public:
-    
-    vector<vector<pair<int,int>>> graph;
-    vector<int> dis;
-    
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        
-        graph.resize(n);
-        dis.resize(n,INT_MAX);
-        
-        for(int i = 0 ; i<flights.size() ; i++){
-            
-            int u = flights[i][0];
-            int v = flights[i][1];
-            int w = flights[i][2];
-            
-            graph[u].push_back({v,w});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+        distance.resize(n,INT_MAX);
+        for(auto i: flights)
+        {
+            adjlist[i[0]].push_back({i[1],i[2]});
         }
+        distance[src]=0;
         
-        queue<tuple<int,int,int>> pq;
-        pq.push({0, src , 0});      //  dis , source , no of stops
-         
-        dis[src] = 0;
-        
-        while(!pq.empty()){
-            
-            auto [d,u,st] = pq.front();
-            pq.pop();
-            
-            
-            for(auto [v , w] : graph[u]){
-                
-                if(d + w < dis[v]  && st<=k){
-            
-                    dis[v] = d + w;
-                    
-                    pq.push({dis[v], v, st+1});
+        q_obj.push({0,src,0}); //distance,vertex,city in between
+        while(q_obj.empty()!=true)
+        {
+            auto [d,u,s]=q_obj.front();
+            q_obj.pop();
+            for(auto i:adjlist[u])
+            {
+                int v=i.first;
+                int w=i.second;
+                if(distance[v]>d+w && s<=k)
+                {
+                    distance[v]=d+w;
+                    q_obj.push({distance[v],v,s+1});
                 }
-                
             }
         }
-        
-        if(dis[dst]==INT_MAX)return -1;
-        
-        return dis[dst];
-        
+        if(distance[dst]==INT_MAX)
+        {
+            return -1;
+        }
+        return distance[dst];
     }
-    
-   
-    
 };
