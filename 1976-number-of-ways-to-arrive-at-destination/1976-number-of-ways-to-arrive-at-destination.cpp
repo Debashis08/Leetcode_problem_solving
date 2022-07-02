8d03fac8f5a6ded59eedd8333d3e67217fcd4852
Debashis Nandi <debashisnandi576@gmail.com>
2022-07-03 00:59:02 +0530

    Time: 97 ms (83.94%), Space: 35.9 MB (11.77%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,7 +1,6 @@
 #define lli long long int
-lli mod=1e9+7;
-
 class Solution {
+    lli mod=1e9+7;
     map<lli,vector<pair<lli,lli>>> adjlist;
     vector<lli> distance;
     vector<lli> dp;
@@ -20,38 +19,19 @@ public:
         dp[0]=1;
         while(pq.empty()!=true)
         {
-            // pair<lli,lli> temp=pq.top();
-            // pq.pop();
-            // for(auto i:adjlist[temp.second])
-            // {
-            //     if(distance[i.first]>distance[temp.second]+i.second)
-            //     {
-            //         distance[i.first]=distance[temp.second]+i.second;
-            //         dp[i.first]=dp[temp.second]%mod;
-            //         pq.push({distance[i.first],i.first});
-            //     }
-            //     else if(distance[i.first]==distance[temp.second]+i.second)
-            //     {
-            //         dp[i.first]=(dp[i.first]%mod + dp[temp.second]%mod)%mod;
-            //     }
-            // }
-            pair<lli,lli> ele=pq.top();
+            pair<lli,lli> temp=pq.top();
             pq.pop();
-            lli last_dis=ele.first;
-            lli last_node=ele.second;
-            for(auto pr:adjlist[last_node])
+            for(auto i:adjlist[temp.second])
             {
-                lli dest=pr.first;
-                lli time=pr.second;
-                if(last_dis+time<distance[dest])
+                if(distance[i.first]>temp.first+i.second)
                 {
-                    distance[dest]=last_dis+time;
-                    dp[dest]=dp[last_node]%mod;
-                    pq.push({last_dis+time,dest});
+                    distance[i.first]=distance[temp.second]+i.second;
+                    dp[i.first]=dp[temp.second]%mod;
+                    pq.push({distance[i.first],i.first});
                 }
-                else if(distance[dest]==time+last_dis)
+                else if(distance[i.first]==temp.first+i.second)
                 {
-                    dp[dest]=(dp[dest]%mod + dp[last_node]%mod)%mod;
+                    dp[i.first]=(dp[i.first]%mod + dp[temp.second]%mod)%mod;
                 }
             }
         }
